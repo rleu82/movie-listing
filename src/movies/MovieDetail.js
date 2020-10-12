6aa620607e57c55c18dccbc81af0d684a5c3e31a
Robert Leu <robertleu@gmail.com>
2020-10-11 17:02:47 -0700

    Added CSS for movie details page

--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom';
 
 const API_KEY = '?api_key=' + process.env.REACT_APP_MOVIES_KEY;
 const API_URL = `https://api.themoviedb.org/3/movie/`;
-
+const IMAGE_URL = 'https://image.tmdb.org/t/p/';
+const BACKDROP_SIZE = 'original';
+const POSTER_SIZE = 'w342';
 export function MovieDetail() {
     const { id } = useParams();
     const [movie, setMovie] = useState({});
@@ -26,13 +28,27 @@ export function MovieDetail() {
 
     return (
         <div>
-            <h1>{movie.title}</h1>
-            <p>{movie.overview}</p>
-            <ul>
-                {movie.genres.map((genre) => (
-                    <li key={genre.id}>{genre.name}</li>
-                ))}
-            </ul>
+            <img
+                className="backdrop"
+                src={IMAGE_URL + BACKDROP_SIZE + movie.backdrop_path}
+                alt={movie.title + ' backdrop'}
+            />
+            <div className="detail-details">
+                <img
+                    className="detail-poster"
+                    src={IMAGE_URL + POSTER_SIZE + movie.poster_path}
+                    alt={movie.title + ' poster'}
+                />
+                <div>
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                    <ul>
+                        {movie.genres.map((genre) => (
+                            <li key={genre.id}>{genre.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
